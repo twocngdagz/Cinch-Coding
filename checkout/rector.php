e2e63c9fffc5ae825d5931e7c393dc4843a4090e
Mederic Roy Beldia <twocngdagz@yahoo.com>
2025-12-08 06:58:48 +0800

    Install testing and analysis tool. Add composer check pipeline for checkout service.

---
+++ checkout/rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([
+        __DIR__.'/vendor',
+    ])
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ]);
---
+++ checkout/tests/Pest.php
@@ -0,0 +1,8 @@
+<?php
+
+pest()->extend(Tests\TestCase::class)
+    ->in('Feature');
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
